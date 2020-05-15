09f3059d5383e892b677a3ff1495997fde3361e5
ddevassy <ddevassy@logitech.com>
2020-05-14 18:32:24 -0700

    Adding npm projects

---
+++ node-coarse/npm_project/index.js
@@ -0,0 +1,14 @@
+var validator = require('validator');
+const chalk = require('chalk');
+const name = require('./util.js')
+
+console.log('val is ' + name)
+const result = validator.isEmail(name);
+
+console.log('email ID Status is ' + result)
+
+const new_result = validator.isBoolean(name)
+
+console.log('Boolean Status is ' + result)
+
+console.log(chalk.green('Hello world!'));
---
+++ node-coarse/npm_project/util.js
@@ -0,0 +1,7 @@
+// console.log('utils.js')
+
+// const emailid = '.disencd@gmail.com'
+const val = '1'
+
+// module.exports = emailid
+module.exports = val
